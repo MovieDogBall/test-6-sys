21b60fc6bb2ddcce0d452d16767d1066eccbbca1
Denys Kashyrin <denys.kashyrin@sixt.com>
2016-11-15 00:05:29 +0200

    update

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ use Building\Building as Building;
 
 $test = new Building();
 $currentFloor = 2; //Этаж где лифт
-$requestFloor = 1; //Этаж с которого вызвали
+$requestFloor = 2; //Этаж с которого вызвали
 $direction = "up"; //направление
 $door = false; //состояние двери
 $active = true; //лифт сейчас в движении
-$queue = array(4, 5, 6, 8); //очередь из этажей по которым должен проехать лифт
+$queue = array(1, 4, 5, 6, 8); //очередь из этажей по которым должен проехать лифт
 
 print_r($test->callElevator($currentFloor, $requestFloor, $direction, $active, $door, $queue));
